54511c8f8d2ef21cdf448226a0d18d49225517b7
zhuojie@sxmaps.net <zhuojie4835@163.com>
2020-07-06 17:43:59 +0800

    简单框架搭建 配置文件（未封装） 日志模块配置

---
+++ API_TEST_FRAME/samples/log_demo.py
@@ -0,0 +1,28 @@
+import logging
+import os
+import time
+
+getLogger = logging.getLogger()         # 创建一个getLogger对象，括号中可以选择带name参数，不输入默认为root
+getLogger.setLevel(logging.WARNING)     # 设置打印日志的级别，大于等于warning级别的日志会打印出来
+
+daytime = time.strftime("%Y-%m-%d")     # 获取当前天数
+log_path = os.path.join(os.getcwd(),  '..', 'logs', daytime + 'test_log.txt')       # 拼接路径和文件名，将日志打印只logs目录下面
+# print(log_path)
+
+handler1 = logging.StreamHandler()      # 打印日志至命令行
+handler2 = logging.FileHandler(log_path,'a',encoding='utf-8')        # 打印日志至test_log文件中，a表示追加
+
+formatter =  logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s")    # 设置打印日志的格式
+handler1.setFormatter(formatter)        # 将handler1的日志格式设置为formatter
+handler2.setFormatter(formatter)
+getLogger.addHandler(handler1)          # 将handler1添加至getLogger对象中
+getLogger.addHandler(handler2)
+
+
+logging.debug('这是一个debug日志信息')
+logging.info('这是一个info日志信息')
+logging.warning('这是一个warning日志信息')
+logging.error('这是一个error日志信息')
+logging.critical('这是一个critical日志信息')
+
+
--- API_TEST_FRAME/samples/read_excel.py
+++ API_TEST_FRAME/samples/read_excel.py
@@ -1,1 +1,7 @@
-print(12)
+import os
+import xlrd
+
+excel_path = os.path.join(os.getcwd(), '..', 'test_case\\test.xlsx')
+print(excel_path)
+workbook = xlrd.open_workbook(excel_path)   # 创建一个工作簿
+# workbook.
