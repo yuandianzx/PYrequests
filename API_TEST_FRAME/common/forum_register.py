ec78e88ff1a62033250b04b7aadf3de5100437ce
zhuojie@sxmaps.net <zhuojie4835@163.com>
2020-07-11 19:02:53 +0800

    20200711微信公众号创建接口

--- API_TEST_FRAME/common/forum_register.py
+++ API_TEST_FRAME/common/forum_register.py
@@ -1,24 +1,18 @@
 import requests
 
 session = requests.session()
-header = {"Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+headers = {"Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
               "Accept-Encoding": "gzip,deflate",
               "User-Agent": "Mozilla/5.0(Windows NT 6.1;Win64;x64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / 83.0.4103.116Safari / 537.36",
               "Accept": "text / html, application / xhtml + xml, application / xml;q = 0.9, image / webp, image / apng, * / *;q = 0.8, application / signed - exchange;v = b3;q = 0.9"
 
               }
 url1 = 'http://47.107.178.45/phpwind/'
-a = session.get(url1, headers = header)
+a = session.get(url1, headers = headers)
 
 
 def forum_register():
     url = 'http://47.107.178.45/phpwind/index.php'
-    header = {"Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-              "Accept-Encoding": "gzip,deflate",
-              "User-Agent": "Mozilla/5.0(Windows NT 6.1;Win64;x64) AppleWebKit / 537.36(KHTML, likeGecko) Chrome / 83.0.4103.116Safari / 537.36",
-              "Accept": "text / html, application / xhtml + xml, application / xml;q = 0.9, image / webp, image / apng, * / *;q = 0.8, application / signed - exchange;v = b3;q = 0.9"
-
-              }
     params = {
             'm' : 'u' ,
             'c' : 'register',
@@ -32,7 +26,7 @@ def forum_register():
         'csrf_token':'2ab91b36e1b33513'
 
             }
-    se = session.post(url, headers = header, params = params,data = data)
+    se = session.post(url, headers = headers, params = params,data = data)
     return se
 
 if __name__ == '__main__':
---
+++ API_TEST_FRAME/common/wechat.py
@@ -0,0 +1,57 @@
+import requests
+from API_TEST_FRAME.common.config_utils import configUtils
+
+session = requests.session()    # 创建session() 对象
+headers = {'Content-Type': 'application/json;charset=UTF-8'}
+
+def get_access_token_api(grant_type, appid, secret):
+    url = configUtils.read_config('host','wechat_host') + 'cgi-bin/token'
+    params = {'grant_type' : grant_type,    # client_credential
+            'appid' : appid,     # wx7cc16178655bc79e
+            'secret': secret}     # ed3e70fe8244e88ff52d3abc2101c31d
+    response = session.get(url, headers = headers, params = params)
+    return response
+'''
+def get_access_token(grant_type, appid, secret):
+    access_token = get_access_token_api(grant_type, appid, secret).json()['access_token']       # 需要转换成json串才能截取token
+    return access_token
+'''
+
+def add_tag_api(access_token, tag):         # 创建标签
+    url = configUtils.read_config('host','wechat_host') + 'cgi-bin/tags/create'
+    params = {'access_token': access_token}
+    data ={'tag' : tag}      # {'id':134, 'name':'广东'}
+    response = session.post(url, headers = headers, params = params, json = data)
+    return response
+
+def edit_tag_api(access_token, tag):        # 编辑标签
+    url = configUtils.read_config('host', 'wechat_host') + 'cgi-bin/tags/update'
+    params = {'access_token': access_token}
+    data = {'tag': tag}
+    response = session.post(url, headers = headers, params = params, json = data)
+    return response
+
+def delete_tag_api(access_token, tag):      # 删除标签
+    url = configUtils.read_config('host', 'wechat_host') + 'cgi-bin/tags/delete'
+    params = {'access_token': access_token}
+    data = {'tag': tag}
+    response = session.post(url, headers = headers, params = params, json = data)
+    return response
+
+def set_use_remark_api(access_token, appid, remark):      # 设置用户备注名
+    url = configUtils.read_config('host', 'wechat_host') + 'cgi-bin/user/info/updateremark'
+    params = {'access_token': access_token}
+    data = {'openid':appid,
+	        'remark':remark}
+    response = session.post(url, headers=headers, params=params, json=data)
+    return response
+
+if __name__ == '__main__':
+    access_token_response = get_access_token_api('client_credential', 'wx7cc16178655bc79e', 'ed3e70fe8244e88ff52d3abc2101c31d')
+    access_token = access_token_response.json()['access_token']
+    print(access_token)
+    create_tag_response = add_tag_api(access_token, {'id':138, 'name':'广东4'})
+    print(create_tag_response.json())
+    # print(add_tag_response.text)
+    # access_token = get_access_token('client_credential', 'wx7cc16178655bc79e', 'ed3e70fe8244e88ff52d3abc2101c31d')
+    # print(access_token)
